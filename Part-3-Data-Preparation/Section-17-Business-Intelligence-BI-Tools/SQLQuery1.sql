48b399afe0cca77a135afab0db9c136a010ed235
AHM <70359375+PhysTony@users.noreply.github.com>
2020-09-08 01:33:50 -0500

    Data Preparation Added

---
+++ Part-3-Data-Preparation/Section-17-Business-Intelligence-BI-Tools/SQLQuery1.sql
@@ -0,0 +1,10 @@
+USE DSTraining; 
+
+CREATE TABLE Test_Table
+(
+	Name VARCHAR(50),
+	Age INT
+)
+
+INSERT INTO Test_Table(Name, Age)
+VALUES('Matt',35),('Jaret',48)
---
+++ Part-3-Data-Preparation/Section-20-Handling-Errors-During-ETL-Phase-1-and-2/Count_Records_FakeNames.sql
@@ -0,0 +1,4 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT
+	Count(*)
+  FROM [DSTraining].[dbo].[RAW_FakeNames_20200906]
